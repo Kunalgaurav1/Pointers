81bb72863e0492feddba8f8a06dfc3434baa9de5
kunalgaurav <kunalgaurav1420@gmail.com>
2023-08-25 19:16:37 +0530

    pointers

---
+++ lec_1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a ;
+
+    cout << &a << endl;
+    cout << a << endl;
+
+    int* p = &a;
+
+    cout << p ;
+
+
+    return 0;
+}
