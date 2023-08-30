6e4f1d24a8c78ee603f339e1f542d9a28f1a3a28
kunalgaurav <kunalgaurav1420@gmail.com>
2023-08-30 08:38:04 +0530

    revision

--- dfd.cpp
+++ dfd.cpp
@@ -3,14 +3,49 @@ using namespace std;
 
 int main(){
 
-    float f = 10.5;
-    float p = 2.5;
-    float* ptr = &f;
-    (*ptr)++;
-    *ptr = p;
-    cout << *ptr << endl;
-    cout << f << endl;
-    cout << p << endl;
+
+/*
+
+   int a = 9;
+   int* p = &a;
+   int ** q = &p;
+
+   cout << a << endl;
+   cout << *p << endl;
+   cout << **q << endl;
+
+   a++;
+   cout << a << endl;
+   cout << *p << endl;
+
+   a = 69;
+   cout << a << endl;
+   cout << *p << endl;
+
+   *p = 70;
+   cout << a << endl;
+   cout << *p << endl;
+
+   a++;
+   cout << a << endl;
+   cout << *p << endl;
+
+   (*p)++;
+   cout << a << endl;
+   cout << *p << endl;
+*/
+
+int a = 9;
+int* p = &a;
+int** q = &p;
+
+cout << a << endl;
+cout << p << endl; //address of a
+cout << *p << endl;//value of a
+cout << q << endl; //address of p
+cout << *q << endl; //address of a
+cout << **q << endl; // value of a
+
 
 
     
--- lec_2.cpp
+++ lec_2.cpp
@@ -11,5 +11,23 @@ int main(){
      p += 1;
 
      cout << p << endl;
+
+
+     double d = 3.4556;
+     double * dp = &d;
+
+     cout << dp << endl;
+
+     dp += 1;
+     cout << dp << endl;
+
+     char c = 'e';
+     char *cp = &c;
+
+     cout << cp << endl;
+
+     cp += 1;
+    
+     cout << cp << endl;
     return 0;
 }
